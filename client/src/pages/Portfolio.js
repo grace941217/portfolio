9313108d2fe16cde598ef41417365b19cdf0676f
Grace <grace941217@gmail.com>
2023-02-25 22:37:04 -0500

    project imgs added

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
 // import images
-import Image1 from '../img/portfolio/1.png';
-import Image2 from '../img/portfolio/2.png';
-import Image3 from '../img/portfolio/3.png';
-import Image4 from '../img/portfolio/4.png';
+import Image1 from '../img/portfolio/Hello World.png';
+import Image2 from '../img/portfolio/NASA Playground.png';
+import Image3 from '../img/portfolio/Flatiron Books.png';
+import Image4 from '../img/portfolio/ComingSoon.jpg';
 // import link
 import { Link } from 'react-router-dom';
 // import motion
